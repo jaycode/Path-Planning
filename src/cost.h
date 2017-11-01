fe36d8ae17ddbc5d9fac96805fa5e1c25c2cc4fd
Jay <teguhwpurwanto@gmail.com>
2017-11-01 18:31:52 +0700

    feat: add some cost functions

--- src/cost.h
+++ src/cost.h
@@ -14,27 +14,61 @@ namespace {
 
     bool IsWrongDirection(const tuple<vector<double>, vector<double>> &traj);
 
-    double CalcOrientationCost(const tuple<vector<double>, vector<double>> &traj) {
+    double OrientationCost(const tuple<vector<double>, vector<double>> &traj) {
       /**
-       * Is the vehicle going in the right direction?
+       * Raises flag when the vehicle goes the opposite direction.
        */
       double cost = 0.0;
       if (IsWrongDirection(traj)) {
         cost = 999.0;
+        // cout << "failed orientation" << endl;
       }
       return cost;
     }
 
-    double CalcSpeedCost(const tuple<vector<double>, vector<double>> &traj,
-                         double target_v,
-                         double dt) {
+    double SpeedCost(const tuple<vector<double>, vector<double>> &traj,
+                     double target_v,
+                     double dt) {
       /**
        * How close is final waypoints' speed to the target speed?
        */
       vector<double> tj_s = get<0>(traj);
       vector<double> state = StateFromTJ(tj_s, dt);
       double v = state[1];
+      // cout << "target_v | v: " << target_v << " | " << v << endl;
       return (fabs(target_v - v));
+    }
+
+    double AccelerationCost(const tuple<vector<double>, vector<double>> &traj,
+                            double max_at,
+                            double max_an,
+                            double dt) {
+      /**
+       * Raises flag when maximum acceleration is higher than the allowed limit.
+       */
+      vector<double> tj_s = get<0>(traj);
+      vector<double> tj_d = get<1>(traj);
+      double cost = 0.0;
+      for (int i = 2; i < (int)tj_s.size(); ++i) {
+        double at = acceleration(tj_s[i], tj_d[i],
+                                 tj_s[i-1], tj_d[i-1],
+                                 tj_s[i-2], tj_d[i-2],
+                                 dt);
+        if (at > max_at) {
+          cost = 999.0;
+          cout << "failed acceleration limit (" << at << " > " << max_at << ")" << endl;
+          break;
+        }
+      }
+      return cost;
+    }
+
+    double ChangeLaneCost(const vector<double> initial_state, double target_d) {
+      /**
+       * Incur a small cost for changing lane.
+       */
+
+      return(fabs(initial_state[3] - target_d));
     }
 
     bool IsWrongDirection(const tuple<vector<double>, vector<double>> &traj) {
@@ -76,7 +110,7 @@ namespace {
 
     }
 
-    void testCalcSpeedCost() {
+    void testSpeedCost() {
 
       tuple<vector<double>, vector<double>> traj;
       vector<double> tj_s = {1.0, 3.0, 3.5};
@@ -87,7 +121,11 @@ namespace {
       get<0>(traj) = tj_s;
       get<1>(traj) = tj_d;
 
-      assert(CalcSpeedCost(traj, target_v, dt) == 0.0);
+      assert(SpeedCost(traj, target_v, dt) == 0.0);
+
+    }
+
+    void testAccelerationCost() {
 
     }
   }
